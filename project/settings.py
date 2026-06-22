a73361aa38a9ab386ceac45a5bdb7f0b3b6f250b
chelladevi-06 <chelladevithevan@gmail.com>
2026-06-22 18:52:51 +0530

    Deployment setup

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1q4gdg0__m0lzf0(lf@ocg_)p)s1rrdkd)r&yt^b&v%morm1h)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
